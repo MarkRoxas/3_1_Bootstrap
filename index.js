8f7d4c5885e7c4fd8497512c037d1caf5f61cefe
MarkRoxas <gaberoxas5721@gmail.com>
2023-11-16 16:11:07 +0800

    Added changes

--- index.js
+++ index.js
@@ -1,70 +1,125 @@
+console.log("hello world");
+// let btnRegister = document.getElementById("btnRegister");
 let btnRegister = document.querySelector("#btnRegister");
-console.log(btnRegister);
+let btnLogin = document.querySelector("#btnLogin");
+let btnLogout = document.querySelector("#btnRegister");
 
-const register = () => {
-    let txtName = document.querySelector("#txtRegisterName");
-    let txtEmail = document.querySelector("#txtRegisterAddress");
-    let txtPassword = document.querySelector("#txtRegisterPassword");
-    let txtConfirmPassword = document.querySelector("#txtRegConfirmPass");
-    let txtModalName = document.querySelector('#txtModalName');
-    const genderRadios = document.getElementsByName('radioGender');
-    const selectElement = document.getElementsByName('#floatingSlect');
-    const txtArea = document.getElementsByName('#txtAdd');
-    const modalDate = document.getElementsByName('#txtRegisterBirthdate');
-
-    let selectedGende;
-    for (const radio of genderRadios){
-        if (radio.checker){
-            selectGender = radio.value;
-            break;
-        }
-    }
-
-const selectedOption = selectElement.options[selectElement.selectedIndex];
-
-const selectedValue = selectedOption.value;
-
-    let user = {
-        name: txtName.value,
-        email: txtEmail.value,
-        password: txtPassword.value,
-        confirmPassword: txtConfirmPassword.value,
-        gender: selectedGender,
-        yearLvl: selectedValue,
-        address: txtArea.value,
-        birthdate: modalDate.value,
-    }
-
-    if(!user.email){
-        return alert('Email address is required.')
-    } else if (!user.name){
-        return alert('Name is required!')
-    } else if (!user.password || !user.confirmPassword){
-        return alert("Password is required.")
-    } else if (user.password != user.confirmPassword){
-        return alert('Password does not match.')
-    } else if (!user.gender){
-        return alert('Gender is required!')
-    } else if (user.yearLvl == 0){
-        return alert('Year lvl is required!')
-    } else if (!user.address){
-        return alert('Address is required!')
-    } else if (!user.birthdate){
-        return alert('Birthdate is required!')
-    }
-    console.log(user.yearLvl);
-    
-alert(`Welcome ${user.name}. You have been successfully registered.`)
-console.log(user);
-
-        localStorage.setItem("user", JSON.stringify(user));
+//console.log(btnRegister);
+function getUsers(){
+    let users = localStorage.getItem("users");
+    if (users != null) users = JSON.parse(users);
+    else users = null;
+    return users;  
 }
 
+function registerUser(){
+    let name = document.querySelector("#txtRegister");
+    let email = document.querySelector("#txtemail");
+    let password = document.querySelector("#txtPassword");
+    let confirmPassword = document.querySelector("#txtConfirmPassword");
+    
+    let users = getUsers();
 
-let btnModalRegister = document.querySelector('#btnRegister')
-btnRegister.addEventListener('click', () => {
-    register()
-})
+    let user = {
+        name:name.value,
+        email:email.value,
+        password:password.value,
+        confirmPassword:confirmPassword.value
+    }
+    typeof user;
+    console.log("button register is clicked", user);
+    
+    var form_check = document.getElementById('form-check').value;
+    var radioGender;
+    if(form-check == 'radioGender1'){
+        radioGender = document.getElementById('gender1').value;
+    }else if(form-check == 'radioGender2'){
+        radioGender = document.getElementById('gender2').value;
+    }else if(form-check == 'radioGender3'){
+        radioGender = document.getElementById('gender3').value;
+    }
+    document.getElementById('gender').innerHTML = radioGender;
 
-let user = JSON.parse(localStorage.getItem('user'))
-console.log(`My name is ${user.name} and my email is ${user.email}`);
+    function getTextareaValue() {
+    var textareaElement = document.getElementById("txtaddress");
+    var addressValue = textareaElement.value;
+    console.log("The entered address is: " + addressValue);
+    }
+    function getDateValue() {
+        var dateInput = document.getElementById("floatingDate");
+        var birthdateValue = dateInput.value;
+        console.log("The entered birthdate is: " + birthdateValue);
+    }
+}
+
+if (!user.email)
+    alert("email is required");
+    else if (
+        users.findIndex((u) => {
+            return u.email == user.email;
+        }) != -1
+    ) {
+        return alert ("Email is already taken.");
+}       else if(!user.name){
+    return alert("Name is required");
+}       else if(user.password || user.password) {
+    return alert("password is required");
+}       else if(user.password != user.confirmPassword) {
+    return alert("password does not match");
+
+    if (users != null && users.length > 0) {
+        users.push(user)
+    } else users = [user];
+
+    localStorage.setItem("user", JSON.stringify(user));
+    alert(`User ${user.name} has been successfully registered.`);
+    let btnModalRegisterClose = document.querySelector("#btnModalRegisterClose").click();
+    //btnModalRegisterClose.click()
+
+    name.value = "";
+    email.value = "";
+    password.value = "";
+    confirmPassword.value = "";
+}
+
+function loginUser(email, password){
+    let users = getUsers();
+
+    let userIndex = users.findIndex != -1;
+    let isUserExist = 
+    users.findIndex((u)=>{
+        return u.mail == email && u.password == password;
+    }) != -1;
+
+    if (!isUserExist) 
+        alert('Email or password is invalid.');
+
+    document.querySelector("#containerProducts");
+    document.querySelector("#containerLogin");
+
+    alert(`Welcome ${users[userIndex].name}`)
+    localStorage.setItem("isLogin", true);
+    document.querySelector("#btnLogout").hiddeen = false;
+    
+}
+
+btnRegister.onClick = () => {
+    registerUser();
+    form_check();
+    getTextareaValue();
+    getDateValue();
+}
+btnLogin.onClick = () => {
+    let txtLoginEmail = document.querySelector("#txtLoginEmail");
+    let txtLoginPassword = document.querySelector("#txtLoginPassword");
+    loginUser(txtLoginEmail.value, txtLoginPassword.value);
+};
+
+btnLogout.onClick = () => {
+    localStorage.setItem("isLogin", false);
+    document.querySelector("#btnLogout").hiddeen = true;
+    containerProducts.hiddeen = true;
+    containerProducts.hiddeen = false;
+    document.querySelector("#txtLoginEmail").value = "";
+    document.querySelector("#txtLoginPassword").value = "";
+};
